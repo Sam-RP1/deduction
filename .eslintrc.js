dc58bf000f79ee8ba2a5532592884518189d65de
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-19 21:35:24 +0100

    fix: added react version and babelOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parser": "@babel/eslint-parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+    ecmaVersion: 12,
+    sourceType: "module",
+    babelOptions: {
+      configFile: "./.babelrc"
     }
+  },
+  plugins: [ "react" ],
+  settings: {
+    react: {
+      version: "latest",
+    },
+  },
+  rules: {}
 };
