c8172736549344d5a253f5c8a886814c0e25a902
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-20 16:59:32 +0100

    Created Toggle Switch UI component
    Created the Toggle Switch UI component with its styling.

---
+++ src/frontend/components/UI/ToggleSwitch/ToggleSwitch.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './ToggleSwitch.scss';
+
+const ToggleSwitch = (props) => {
+  const [isToggled, setToggleState] = useState(false);
+
+  const toggle = () => {
+    isToggled ? setToggleState(false) : setToggleState(true);
+  }
+
+  return (
+    <div onClick={() => {props.function(); toggle();}} className={"toggle-switch " + (isToggled ? "active" : "")}>
+      <span></span>
+    </div>
+  )
+}
+
+ToggleSwitch.propTypes = {
+  function: PropTypes.func.isRequired
+}
+
+ToggleSwitch.defaultProps = {
+  function: () => console.log("[TOGGLE SWITCH] undefined function, using default props.")
+};
+
+export default ToggleSwitch;
