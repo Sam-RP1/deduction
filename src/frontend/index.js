80963d4551c7286d3226a10e4b36211233b46b4f
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-20 16:59:54 +0100

    Added react-router-dom

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 // Styles
 import './styles/root.scss';
 
 // Imports
 import App from './App';
-import reducer from './store/reducer';
+import reducer from './store/reducer.js';
 
 // Redux Store
 const store = createStore(reducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// Construct App
+const app = (
+  <Provider store={store}>
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+  </Provider>
+)
+
+// Render
+ReactDOM.render(app, document.getElementById('root'));
