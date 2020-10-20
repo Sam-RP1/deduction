cd9dc60be177c05530354a785e7f8a154eafbc5c
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-20 17:00:49 +0100

    Updated to allow for theme toggling with ToggleSwitch

--- src/frontend/components/Footer/Footer.js
+++ src/frontend/components/Footer/Footer.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import ToggleSwitch from '../UI/ToggleSwitch/ToggleSwitch';
 
 import '../../styles/root.scss';
 import './Footer.scss';
 
-const Footer = () => {
+const Footer = (props) => {
   return (
-    <section className="footer container">
-      <div>
-      <div>
-      <p>Toggle Dark Mode</p>
-      <span>toggle element</span>
-      </div>
+    <section className="footer">
+      <div className="footer__row footer__settings">
+        <div className="footer__settings__toggle-switch-wrapper">
+          <p>Toggle Dark Mode</p>
+          <ToggleSwitch function={props.themeToggle} />
+        </div>
       <i onClick={() => console.log("[BUTTON CLICK] open settings menu")} className="fas fa-cog"></i>
       </div>
 
-      <div>
-      <p>Developed by <a href="http://srenshawpanting.co.uk/"><u>SRP Designs</u></a></p>
-      <p><a href=""><u>Donate here</u></a></p>
+      <div className="footer__row footer__info">
+        <p>Developed by <a href="http://srenshawpanting.co.uk/"><u>SRP Designs</u></a></p>
+        <p><a href=""><u>Donate here</u></a></p>
       </div>
     </section>
   )
+}
+
+Footer.propTypes = {
+  themeToggle: PropTypes.func
 }
 
 export default Footer;
