590d00e7247ca4425dbe2aed54d72f2e41557442
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-20 17:00:17 +0100

    Updated with Toggle-able theme

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Footer from './components/Footer/Footer';
 
 const App = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    isDarkTheme ? setIsDarkTheme(false) : setIsDarkTheme(true);
+    console.log("[THEME] toggled")
+  }
+
   return (
-    <Footer />
+    <div className={"theme " + (isDarkTheme ? "theme--dark" : "theme--default")}>
+    <section className="test"></section>
+    <Footer themeToggle={() => toggleTheme()} />
+    </div>
   );
 }
 
