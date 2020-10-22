a29c3f69b936bc452ce87e6cd66ed9db86a3ed0f
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-22 18:07:17 +0100

    Created loading indicator and styling

---
+++ src/frontend/components/UI/LoadingIndicator/LoadingIndicator.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './LoadingIndicator.scss';
+
+const LoadingIndicator = () => (
+  <div className="loading-indicator">
+  <div></div>
+  <div></div>
+  <div></div>
+
+  <div></div>
+  <span></span>
+  <div></div>
+
+  <div></div>
+  <div></div>
+  <div></div>
+  </div>
+);
+
+export default LoadingIndicator;
