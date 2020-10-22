04799e74974b47c5fe3c13befdafcefba23c242b
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-22 18:07:53 +0100

    minor changes

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
-import ContentContainer from './components/UI/ContentContainer/ContentContainer'
-import MainMenu from './components/MainMenu/MainMenu';
+import ContentContainer from './components/UI/ContentContainer/ContentContainer';
+import MainMenu from './components/MainMenu/MainMenu'; // eslint-disable-line
+import SetupMenu from './components/SetupMenu/SetupMenu'; // eslint-disable-line
+import Deduction from './components/Deduction/Deduction'; // eslint-disable-line
 import Footer from './components/Footer/Footer';
 
 const App = () => {
@@ -16,7 +18,7 @@ const App = () => {
     <div className={"theme " + (isDarkTheme ? "theme--dark" : "theme--default")}>
     <div className="base">
     <ContentContainer opClasses={"content-container__center content-container__column"}>
-    <MainMenu />
+    <Deduction />
     </ContentContainer>
     <Footer themeToggle={() => toggleTheme()} />
     </div>
