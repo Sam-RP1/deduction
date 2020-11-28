71af63f505cecc991e2253f70c8a005adce602a5
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-11-28 15:43:48 +0000

    formatting changes

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,62 +4,70 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/frontend/index.js']
-  },
-  output: {
-    path: path.join(__dirname, 'build'),
-    publicPath: '/',
-    filename: './public/bundles/[name]-bundle.js'
-  },
-  mode: 'development',
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js|jsx$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader"
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          outputPath: './public/assets/images'
-        }
-      },
-      {
-        test: /\.(pdf|md)$/i,
-        loader: 'file-loader',
-        options: {
-          outputPath: './public/assets/files'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new ESLintPlugin({
-      context: 'src/frontend',
-      extensions: ['js']
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/frontend/pages/index.html",
-      filename: "./public/index.html",
-      excludeChunks: [ 'server' ]
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
-}
+    entry: {
+        index: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/frontend/index.js'],
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        publicPath: '/',
+        filename: './public/bundles/[name]-bundle.js',
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './public/assets/images',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(pdf|md)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './public/assets/files',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new ESLintPlugin({
+            context: 'src/frontend',
+            extensions: ['js'],
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/frontend/pages/index.html',
+            filename: './public/index.html',
+            excludeChunks: ['server'],
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+    ],
+};
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,96 +1,96 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: "./src/frontend/index.js"
-  },
-  output: {
-    path: path.join(__dirname, 'build/public/'),
-    filename: 'bundles/[name]-bundle.js',
-     chunkFilename: 'bundles/[name].bundle.js'
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        cache: true,
-        parallel: true,
-        terserOptions: {},
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ],
-    splitChunks: {
-      chunks: 'all',
-      automaticNameDelimiter: '-'
+    entry: {
+        index: './src/frontend/index.js',
     },
-    removeEmptyChunks: true,
-    mergeDuplicateChunks: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js|jsx$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '../',
+    output: {
+        path: path.join(__dirname, 'build/public/'),
+        filename: 'bundles/[name]-bundle.js',
+        chunkFilename: 'bundles/[name].bundle.js',
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                terserOptions: {},
+            }),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+        splitChunks: {
+            chunks: 'all',
+            automaticNameDelimiter: '-',
+        },
+        removeEmptyChunks: true,
+        mergeDuplicateChunks: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
             },
-          },
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|svg|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: './assets/images',
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                        },
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
-          }
-        ]
-      },
-      {
-        test: /\.(pdf|md)$/i,
-        loader: 'file-loader',
-        options: {
-          outputPath: './assets/files',
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/frontend/pages/index.html",
-      filename: "./index.html",
-      excludeChunks: [ 'server' ]
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles/[name].css',
-      chunkFilename: '[id].css'
-    })
-  ]
-}
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './assets/images',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(pdf|md)$/i,
+                loader: 'file-loader',
+                options: {
+                    outputPath: './assets/files',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/frontend/pages/index.html',
+            filename: './index.html',
+            excludeChunks: ['server'],
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+};
