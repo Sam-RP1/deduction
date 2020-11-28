da568c2fc8ccaa5e108dd25d44da84668eda9029
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-11-28 15:43:59 +0000

    formatting changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,33 @@
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  parser: "@babel/eslint-parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        node: true,
+        browser: true,
     },
-    ecmaVersion: 12,
-    sourceType: "module",
-    babelOptions: {
-      configFile: "./.babelrc"
-    }
-  },
-  plugins: [ "react" ],
-  settings: {
-    react: {
-      version: "latest",
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+        babelOptions: {
+            configFile: './.babelrc',
+        },
     },
-  },
-  rules: {}
+    plugins: ['react'],
+    settings: {
+        react: {
+            version: 'latest',
+        },
+    },
+    rules: {},
+    overrides: [
+        {
+            files: ['**/*.spec.js', '**/*.spec.jsx'],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 };
