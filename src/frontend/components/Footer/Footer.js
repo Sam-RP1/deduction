8aa1c49c2f28d573150ae654ac1a85f9f832a28a
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-11-28 15:45:16 +0000

    formatting changes

--- src/frontend/components/Footer/Footer.js
+++ src/frontend/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import ToggleSwitch from '../UI/ToggleSwitch/ToggleSwitch';
@@ -6,26 +7,37 @@ import ToggleSwitch from '../UI/ToggleSwitch/ToggleSwitch';
 import './Footer.scss';
 
 const Footer = (props) => {
-  return (
-    <footer className="footer">
-      <div className="footer__row footer__settings">
-        <div className="footer__settings__toggle-switch-wrapper">
-          <p>Toggle Dark Mode</p>
-          <ToggleSwitch function={props.themeToggle} />
-        </div>
-      <i onClick={() => console.log("[BUTTON CLICK] open settings menu")} className="fas fa-cog"></i>
-      </div>
+    return (
+        <footer className='footer'>
+            <div className='footer__settings footer__settings--row'>
+                <div className='footer__settings__toggle-switch-wrapper'>
+                    <p>Toggle Dark Mode</p>
+                    <ToggleSwitch function={props.themeToggle} />
+                </div>
+                <NavLink to='/settings' onClick={() => console.log('[BUTTON CLICK] open settings menu')}>
+                    <i className='fas fa-cog'></i>
+                </NavLink>
+            </div>
 
-      <div className="footer__row footer__info">
-        <p>Developed by <a href="http://srenshawpanting.co.uk/"><u>SRP Designs</u></a></p>
-        <p><a href=""><u>Donate here</u></a></p>
-      </div>
-    </footer>
-  )
-}
+            <div className='footer__info footer__info--row'>
+                <p>
+                    Developed by{' '}
+                    <a href='http://srenshawpanting.co.uk/'>
+                        <u>SRP Designs</u>
+                    </a>
+                </p>
+                <p>
+                    <a href=''>
+                        <u>Donate here</u>
+                    </a>
+                </p>
+            </div>
+        </footer>
+    );
+};
 
 Footer.propTypes = {
-  themeToggle: PropTypes.func
-}
+    themeToggle: PropTypes.func,
+};
 
 export default Footer;
