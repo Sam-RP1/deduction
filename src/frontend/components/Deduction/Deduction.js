f084f833d77da43ddd2eb9949415c9959b6e749c
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-11-28 15:53:28 +0000

    update: minor changes

--- src/frontend/components/Deduction/Deduction.js
+++ src/frontend/components/Deduction/Deduction.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import LoadingIndicator from '../UI/LoadingIndicator/LoadingIndicator'; // eslint-disable-line
+import Title from '../UI/Title/Title';
 import Gameboard from './Gameboard/Gameboard';
 import GameControls from './GameControls/GameControls';
 
@@ -10,16 +11,18 @@ import './Deduction.scss';
 const Deduction = () => {
   return (
     <section className="deduction">
-    <h1 className="deduction__title">Deduction</h1>
+    <Title />
+    <div className="deduction__gameboard">
     <div className="deduction__gameboard-container">
     <div className="deduction__gameboard-container__information">
     <p>Score: <span className="red-txt">X</span> - <span className="blue-txt">X</span></p>
     <p>Game Time: 6:00</p>
-    <p><span className="blue-txt">Blue's</span> Turn</p>
+    <p><span className="blue-txt">Blue’s</span> Turn</p>
     </div>
     <Gameboard />
     </div>
     <GameControls />
+    </div>
     </section>
   )
 }
