b0eed25520c611a9b70c5b43854660dcb6416b28
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-03 18:21:32 +0000

    update: implemented props to allow the toggle switch to be toggled on its load via props.

--- src/frontend/components/UI/ToggleSwitch/ToggleSwitch.js
+++ src/frontend/components/UI/ToggleSwitch/ToggleSwitch.js
@@ -1,28 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import './ToggleSwitch.scss';
 
 const ToggleSwitch = (props) => {
-  const [isToggled, setToggleState] = useState(false);
+    const [isToggled, setToggleState] = useState(false);
 
-  const toggle = () => {
-    isToggled ? setToggleState(false) : setToggleState(true);
-  }
+    const toggle = () => {
+        isToggled ? setToggleState(false) : setToggleState(true);
+    };
 
-  return (
-    <div onClick={() => {props.function(); toggle();}} className={"toggle-switch " + (isToggled ? "active" : "")}>
-      <span></span>
-    </div>
-  )
-}
+    useEffect(() => {
+        if (props.toggle) {
+            setToggleState(true);
+        }
+    }, []);
+
+    return (
+        <div
+            onClick={() => {
+                props.function();
+                toggle();
+            }}
+            className={'toggle-switch ' + (isToggled ? 'active' : '')}
+        >
+            <span></span>
+        </div>
+    );
+};
 
 ToggleSwitch.propTypes = {
-  function: PropTypes.func.isRequired
-}
+    toggle: PropTypes.bool,
+    function: PropTypes.func.isRequired,
+};
 
 ToggleSwitch.defaultProps = {
-  function: () => console.log("[TOGGLE SWITCH] undefined toggle switch function")
+    toggle: false,
+    function: () => console.log('[TOGGLE SWITCH] undefined toggle switch function'),
 };
 
 export default ToggleSwitch;
