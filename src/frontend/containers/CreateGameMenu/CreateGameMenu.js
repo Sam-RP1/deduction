80294a6495be48386c044c3ff1e6fcde4680e13c
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-03 18:22:54 +0000

    update: minor changes

--- src/frontend/containers/CreateGameMenu/CreateGameMenu.js
+++ src/frontend/containers/CreateGameMenu/CreateGameMenu.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
 // Redux Action Types
-import * as actionTypes from '../../store/actions';
+import * as cgm from '../../store/actions/createGameMenu';
 
 // Presentational Components
 import CreateGameCmpnt from '../../components/Menus/CreateGameMenu/CreateGameMenu';
@@ -11,28 +11,19 @@ import CreateGameCmpnt from '../../components/Menus/CreateGameMenu/CreateGameMen
 // Container Component
 const CreateGameMenu = () => {
     // Redux Selectors
-    const turnTimer = useSelector((state) => state.createGameMenu.turnTimer);
-    const quickGame = useSelector((state) => state.createGameMenu.quickGame);
-    const wordGroup = useSelector((state) => state.createGameMenu.wordGroup);
-    const customWords = useSelector((state) => state.createGameMenu.customWords, shallowEqual);
+    const turnTimer = useSelector((state) => state.cgm.turnTimer);
+    const quickGame = useSelector((state) => state.cgm.quickGame);
+    const wordGroup = useSelector((state) => state.cgm.wordGroup);
+    const customWords = useSelector((state) => state.cgm.customWords, shallowEqual);
 
     // Redux Actions
     const dispatch = useDispatch();
-    const toggleTurnTimer = useCallback(() => dispatch({ type: actionTypes.TOGGLE_TURN_TIMER }), [dispatch]);
-    const toggleQuickGame = useCallback(() => dispatch({ type: actionTypes.TOGGLE_QUICK_GAME }), [dispatch]);
-    const selectWordGroup = useCallback(
-        (id) => dispatch({ type: actionTypes.SELECT_WORD_GROUP, payload: { selectedWordGroup: id } }),
-        [dispatch]
-    );
-    const addCustomWord = useCallback(
-        (word) => dispatch({ type: actionTypes.ADD_CUSTOM_WORD, payload: { word: word } }),
-        [dispatch]
-    );
-    const deleteCustomWord = useCallback(
-        (word) => dispatch({ type: actionTypes.DELETE_CUSTOM_WORD, payload: { word: word } }),
-        [dispatch]
-    );
-    const resetState = useCallback(() => dispatch({ type: actionTypes.RESET_STATE }), [dispatch]);
+    const toggleTurnTimer = useCallback(() => dispatch(cgm.toggleTurnTimer()), [dispatch]);
+    const toggleQuickGame = useCallback(() => dispatch(cgm.toggleQuickGame()), [dispatch]);
+    const selectWordGroup = useCallback((id) => dispatch(cgm.selectWordGroup(id)), [dispatch]);
+    const addCustomWord = useCallback((word) => dispatch(cgm.addCustomWord(word)), [dispatch]);
+    const deleteCustomWord = useCallback((word) => dispatch(cgm.deleteCustomWord(word)), [dispatch]);
+    const submitSettings = useCallback(() => dispatch(cgm.submit()), [dispatch]);
 
     // State
     const [customWordsErrMsg, setCustomWordsErrMsg] = useState(null);
@@ -46,16 +37,16 @@ const CreateGameMenu = () => {
     const wordGroupHandler = (id) => {
         const prevSelected = document.getElementById(wordGroup);
         const curSelected = document.getElementById(id);
-        if (id !== wordGroup && wordGroup !== null) {
+        if (id !== wordGroup && wordGroup !== '') {
             prevSelected.setAttribute('aria-checked', 'false');
             curSelected.setAttribute('aria-checked', 'true');
             selectWordGroup(id);
-        } else if (wordGroup === null) {
+        } else if (wordGroup === '') {
             curSelected.setAttribute('aria-checked', 'true');
             selectWordGroup(id);
         } else if (id === wordGroup) {
             curSelected.setAttribute('aria-checked', 'false');
-            selectWordGroup(null);
+            selectWordGroup('');
         }
     };
 
@@ -80,30 +71,18 @@ const CreateGameMenu = () => {
         }
     };
 
-    const deleteCustomWordHandler = (word) => {
-        if (customWords.indexOf(word) > -1) {
-            deleteCustomWord(word);
-        }
-    };
-
     const submitHandler = () => {
-        if ((wordGroup !== null && customWords.length === 0) || (wordGroup === null && customWords.length === 25)) {
+        if ((wordGroup !== '' && customWords.length === 0) || (wordGroup === null && customWords.length === 25)) {
             console.log('SUBMITTED');
+
+            submitSettings();
             history.push('/game');
-            const gameSettings = {
-                turnTimer: turnTimer,
-                quickGame: quickGame,
-                wordGroup: wordGroup,
-                customWords: customWords,
-            };
-            console.log(gameSettings);
-            resetState();
         } else {
             let errString;
             let optClass = 'err-msg--inherit';
-            if (wordGroup === null && customWords.length !== 25) {
+            if (wordGroup === '' && customWords.length !== 25) {
                 errString = 'You need to either select a word group or enter 25 custom words';
-            } else if (wordGroup !== null && customWords.length > 0) {
+            } else if (wordGroup !== '' && customWords.length > 0) {
                 errString =
                     'You cannot select a word group and have custom words. Please either deselect the selected word group or remove the custom words you have entered';
             }
@@ -127,13 +106,16 @@ const CreateGameMenu = () => {
     // Render
     return (
         <CreateGameCmpnt
+            turnTimer={turnTimer}
             toggleTurnTimer={toggleTurnTimer}
+            quickGame={quickGame}
             toggleQuickGame={toggleQuickGame}
+            selectedWordGroup={wordGroup}
             wordGroups={wordGroups}
             wordGroupHandler={wordGroupHandler}
             customWords={customWords}
             addCustomWordHandler={addCustomWordHandler}
-            deleteCustomWordHandler={deleteCustomWordHandler}
+            deleteCustomWord={deleteCustomWord}
             customWordsErrMsg={customWordsErrMsg}
             submitHandler={submitHandler}
             submitErrMsg={submitErrMsg}
