2b6244e361ca2d9b5c2ddd70020431d7fd84586e
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-08 19:11:33 +0000

    update: minor changes

--- src/frontend/store/reducers/game.js
+++ src/frontend/store/reducers/game.js
@@ -1,27 +1,41 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
+    created: null,
+    gameTimer: null,
+    guessesBlue: [],
+    guessesRed: [],
+    id: null,
+    lastQuery: null,
+    quickGame: null,
     score: {
-        red: null,
         blue: null,
+        red: null,
     },
-    gameTime: null,
     turn: null,
     turnTimer: false,
-    quickGame: false,
-    joinLink: null,
     team: null,
     role: null,
-    words: null,
+    words: [],
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.SET_GAME_SETTINGS:
+        case actionTypes.SET_GAME_SETTINGS: {
+            const gameSettings = action.payload.gameSettings;
             return {
-                ...initialState,
-                gameSettings: action.payload.gameData,
+                ...state,
+                created: gameSettings.created,
+                gameTimer: gameSettings.gameTimer,
+                id: gameSettings.id,
+                lastQuery: gameSettings.lastQuery,
+                quickGame: gameSettings.quickGame,
+                score: gameSettings.score,
+                turn: gameSettings.turn,
+                turnTimer: gameSettings.turnTimer,
+                words: gameSettings.words,
             };
+        }
         default:
             return state;
     }
