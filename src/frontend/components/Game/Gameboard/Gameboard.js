31a6a4c272187959cfbda1a806f5d2c591e39057
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-08 19:10:29 +0000

    update: Gameboard now displays the correct words generated for each game

--- src/frontend/components/Game/Gameboard/Gameboard.js
+++ src/frontend/components/Game/Gameboard/Gameboard.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+// Styles
 import './Gameboard.scss';
 
-const Gameboard = () => {
-  const wordBlocks = [
-    "word", "word2", "word3", "word4", "word5",
-    "word", "word2", "word3", "word4", "word5",
-    "word", "word2", "word3", "word4", "word5",
-    "word", "word2", "word3", "word4", "word5",
-    "word", "word2", "word3", "word4", "word5",
-  ] // eslint-disable-line
-  return (
-    <section className="gameboard">
-    {wordBlocks.map((block, i) => {
-      return (
-        <div key={i} className={"gameboard__word-block gameboard__word-block" + "__large"}>
-        <p>{block}</p>
-        </div>
-      )
-    })}
-    </section>
-  )
-}
+// Presentational Component
+const Gameboard = (props) => {
+    return (
+        <section className='gameboard'>
+            {props.wordsArr.map((block) => {
+                return (
+                    <div
+                        key={block.word}
+                        id={block.word}
+                        className={'gameboard__word-block gameboard__word-block' + '__large'}
+                    >
+                        <p>{block.word}</p>
+                    </div>
+                );
+            })}
+        </section>
+    );
+};
+
+Gameboard.propTypes = {
+    wordsArr: PropTypes.array.isRequired,
+};
+
+Gameboard.defaultProps = {
+    wordsArr: [{ denomination: 'blank', word: 'ERROR' }],
+};
 
 export default Gameboard;
