13715bf9f005ae640efaa5d2c44a5e4a6c84eaea
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-08 19:11:00 +0000

    update: added conditional rendering based on the isLoaded prop

--- src/frontend/components/Game/Game.js
+++ src/frontend/components/Game/Game.js
@@ -1,34 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+// Imports
 import LoadingIndicator from '../UI/LoadingIndicator/LoadingIndicator'; // eslint-disable-line
 import Title from '../UI/Title/Title';
 import Gameboard from './Gameboard/Gameboard';
 import GameControls from './GameControls/GameControls';
 
+// Styles
 import '../../styles/root.scss';
 import './Game.scss';
 
-const Deduction = () => {
+// Presentational Component
+const Game = (props) => {
     return (
         <section className='game'>
             <Title />
-            <div className='game__board'>
-                <div className='game__board-container'>
-                    <div className='game__board-container__information'>
-                        <p>
-                            Score: <span className='red-txt'>X</span> - <span className='blue-txt'>X</span>
-                        </p>
-                        <p>Game Time: 6:00</p>
-                        <p>
-                            <span className='blue-txt'>Blue’s</span> Turn
-                        </p>
+            {props.isLoaded && <LoadingIndicator />}
+            {!props.isLoaded && (
+                <div className='game__board'>
+                    <div className='game__board-container'>
+                        <div className='game__board-container__information'>
+                            <p>
+                                Score: <span className='red-txt'>X</span> - <span className='blue-txt'>X</span>
+                            </p>
+                            <p>Game Time: 6:00</p>
+                            <p>
+                                <span className='blue-txt'>Blue’s</span> Turn
+                            </p>
+                        </div>
+                        <Gameboard wordsArr={props.wordsArr} />
                     </div>
-                    <Gameboard />
+                    <GameControls />
                 </div>
-                <GameControls />
-            </div>
+            )}
         </section>
     );
 };
 
-export default Deduction;
+Game.propTypes = {
+    isLoaded: PropTypes.bool.isRequired,
+    wordsArr: PropTypes.array.isRequired,
+};
+
+Game.defaultProps = {
+    isLoaded: false,
+    wordsArr: [{ denomination: 'blank', word: 'ERROR' }],
+};
+
+export default Game;
