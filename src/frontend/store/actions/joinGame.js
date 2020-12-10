9e22222aaf9fd1c8a47c0653f6dbcdb2952f7e3d
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-10 18:16:30 +0000

    update: joinGame and joinGameAction created for joining games

--- src/frontend/store/actions/joinGame.js
+++ src/frontend/store/actions/joinGame.js
@@ -1,5 +1,13 @@
 import * as actionTypes from './actionTypes';
 
-export const setLink = (link) => {
-    return { type: actionTypes.SET_LINK, payload: { link: link } };
+export const joinGameAction = (data) => {
+    return { type: actionTypes.SET_GAME_SETTINGS, payload: { gameSettings: data } };
+};
+
+export const joinGame = (gameId) => async (dispatch) => {
+    const url = 'https://us-central1-deduction-158f9.cloudfunctions.net/deduction/join/' + gameId;
+    const response = await fetch(url, { method: 'GET' });
+    const data = await response.json();
+    console.log(data);
+    dispatch(joinGameAction(data));
 };
