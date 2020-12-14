f7fc4b6bb045db1a73e7cfaf1a18cf4ac2ab98e9
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-14 17:52:02 +0000

    update: added /refresh route

--- functions/api/create/index.js
+++ functions/api/create/index.js
@@ -1,5 +1,6 @@
 const CreateRouter = require('express').Router();
 
 CreateRouter.route('/').post(require('./create.js'));
+CreateRouter.route('/refresh').post(require('./create.js'));
 
 module.exports = CreateRouter;
