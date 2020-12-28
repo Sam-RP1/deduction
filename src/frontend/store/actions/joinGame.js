1ea9c5fee35a3b595162c0b3a3a097e950271773
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-28 19:32:33 +0000

    update: minor changes to joinGame method

--- src/frontend/store/actions/joinGame.js
+++ src/frontend/store/actions/joinGame.js
@@ -1,13 +1,18 @@
 import * as actionTypes from './actionTypes';
 
-export const joinGameAction = (data) => {
+export const joinGameAC = (data) => {
     return { type: actionTypes.SET_GAME_SETTINGS, payload: { gameSettings: data } };
 };
 
-export const joinGame = (gameId) => async (dispatch) => {
-    const url = 'https://us-central1-deduction-158f9.cloudfunctions.net/deduction/api/game/' + gameId;
+export const joinGame = (data) => async (dispatch) => {
+    const url = 'http://localhost:4000/api/game/' + data.gameId + '/' + data.gamePassword;
     const response = await fetch(url, { method: 'GET' });
-    const data = await response.json();
-    console.log(data);
-    dispatch(joinGameAction(data));
+    const resData = await response.json();
+
+    if (resData.status === 'success') {
+        dispatch(joinGameAC(resData.data));
+        return { status: resData.status };
+    } else {
+        return { status: resData.status, msg: resData.msg };
+    }
 };
