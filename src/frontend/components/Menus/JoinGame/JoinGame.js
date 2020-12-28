0148182c3d6c90a3f1532494b7e110c20b98b8b0
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-28 19:36:10 +0000

    update: added loading indicator

--- src/frontend/components/Menus/JoinGame/JoinGame.js
+++ src/frontend/components/Menus/JoinGame/JoinGame.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Imports
+import LoadingIndicator from '../../UI/LoadingIndicator/LoadingIndicator';
 import Title from '../../UI/Title/Title';
 import Button from '../../UI/Buttons/Button/Button';
 
@@ -13,26 +14,62 @@ const JoinGame = (props) => {
     return (
         <section className='join-game'>
             <Title title='Deduction' />
+            {props.isLoading ? (
+                <LoadingIndicator />
+            ) : (
+                <>
+                    <div className='join-game__content'>
+                        <h3>Join an existing game:</h3>
+                        <p>
+                            You can join a game by either entering a valid join link OR entering the games name and
+                            password.
+                        </p>
+                        <label htmlFor='game-join-link'>Game Join Link:</label>
+                        <input
+                            ref={props.joinLinkRef}
+                            name='game-join-link'
+                            type='password'
+                            autoComplete='off'
+                            maxLength='61'
+                        />
+                        {props.joinLinkError}
+                        <p>OR</p>
+                        <label htmlFor='game-id'>Game Name:</label>
+                        <input ref={props.gameNameRef} name='game-id' autoComplete='off' maxLength='20' />
+                        {props.gameNameError}
+                        <label htmlFor='game-password'>Game Password:</label>
+                        <input
+                            ref={props.gamePasswordRef}
+                            name='game-password'
+                            type='password'
+                            autoComplete='off'
+                            maxLength='20'
+                        />
+                        {props.gamePasswordError}
+                        <p>-</p>
+                        <label htmlFor='player-name'>Player Name:</label>
+                        <input ref={props.playerNameRef} name='player-name' autoComplete='off' maxLength='12' />
+                        {props.playerNameError}
+                        {props.generalError}
+                    </div>
 
-            <div className='join-game__content'>
-                <h3>Join an existing game:</h3>
-                <label htmlFor='join-link'>Lobby Join Link:</label>
-                <input ref={props.joinLinkRef} name='join-link' type='password' autoComplete='off' maxLength='20' />
-                {props.joinLinkError}
-                <label htmlFor='player-name'>Player Name:</label>
-                <input ref={props.playerNameRef} name='player-name' autoComplete='off' maxLength='12' />
-                {props.playerNameError}
-            </div>
-
-            <Button title={'Join!'} function={props.submitHandler} />
+                    <Button title={'Join!'} function={props.submitHandler} />
+                </>
+            )}
         </section>
     );
 };
 
 JoinGame.propTypes = {
+    isLoading: PropTypes.bool,
     joinLinkRef: PropTypes.object,
+    gameNameRef: PropTypes.object,
+    gamePasswordRef: PropTypes.object,
     playerNameRef: PropTypes.object,
+    generalError: PropTypes.element,
     joinLinkError: PropTypes.element,
+    gameNameError: PropTypes.element,
+    gamePasswordError: PropTypes.element,
     playerNameError: PropTypes.element,
     submitHandler: PropTypes.func.isRequired,
 };
