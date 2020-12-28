f7499310766d8386b686b1a98fe68783a0f27a38
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-28 19:30:36 +0000

    update: created getWordBundles and generateWords methods

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -2,6 +2,8 @@
 
 //------------- GLOBAL VARIABLES -------------//
 const mysql = require('mysql2/promise');
+const _ = require('underscore');
+const wordGroups = require('../appdata/wordGroups.js');
 require('dotenv').config();
 
 const dbData = {
@@ -9,6 +11,51 @@ const dbData = {
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: 'deduction_DB',
+};
+
+module.exports.getWordBundles = () => {
+    const keys = Object.keys(wordGroups);
+    const idArray = [];
+    for (let i = 0; i < keys.length; i++) {
+        idArray.push(wordGroups[keys[i]].id);
+    }
+    return idArray;
+};
+
+module.exports.generateWords = (bundleId) => {
+    const bundleWords = wordGroups[bundleId].words;
+    const wordsArr = [];
+    let words = _.sample(bundleWords, 25);
+
+    // Allocate words between red, blue, blank and bomb
+    const redWords = _.sample(words, 9);
+    const noRedWords = words.filter((word) => !redWords.includes(word));
+    const blueWords = _.sample(noRedWords, 8);
+    const noBlueRedWords = noRedWords.filter((word) => !blueWords.includes(word));
+    const blankWords = _.sample(noBlueRedWords, 7);
+    const bombWord = noBlueRedWords.filter((word) => !blankWords.includes(word));
+
+    // Push each denomination of words into an array
+    for (let i = 0; i < 9; i++) {
+        wordsArr.push({ denomination: 'red', word: redWords[i] });
+    }
+    for (let i = 0; i < 8; i++) {
+        wordsArr.push({ denomination: 'blue', word: blueWords[i] });
+    }
+    for (let i = 0; i < 7; i++) {
+        wordsArr.push({ denomination: 'blank', word: blankWords[i] });
+    }
+    wordsArr.push({ denomination: 'bomb', word: bombWord[0] });
+
+    // Shuffle word array
+    const shuffledWords = _.shuffle(wordsArr);
+
+    // Assign words index numbers (0-24)
+    for (let i = 0; i < shuffledWords.length; i++) {
+        shuffledWords[i].index = i;
+    }
+
+    return shuffledWords;
 };
 
 module.exports.sqlPromise = mysql.createConnection(dbData);
