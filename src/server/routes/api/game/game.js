9afff691a92da218a77b51e08cdf8d5544495d8c
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-28 19:29:25 +0000

    update: added game.get and update game.post

--- src/server/routes/api/game/game.js
+++ src/server/routes/api/game/game.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const randomstring = require('randomstring');
 const dbGame = require('../../../db/db-game/db-game');
-const { SUCCESS, FAIL, EXISTS } = require('../../../config/statusTypes');
+const { SUCCESS, FAIL, EMPTY } = require('../../../config/statusTypes');
+const { getWordBundles } = require('../../../config/config.js');
 
 const game = express.Router();
 
-// const io = game.get('socketIO'); didnt work
+// const io = game.get('socketIO'); dw
+// const io = req.app.locals.io;
+// io.emit('conc', { data: 'data' });
 
 module.exports = game;
 
@@ -15,37 +17,52 @@ game.use(cors({ origin: true }));
 game.use(bodyParser.json());
 game.use(bodyParser.urlencoded({ extended: true }));
 
-game.post('/', async (req, res) => {
+game.get('/:gameid/:gamepassword', async (req, res) => {
     try {
-        const gameName = req.body.gameName;
-        const gamePassword = req.body.gamePassword;
-        const playerName = req.body.playerName;
-        const randStrLength = 29 - gameName.length;
+        const gameId = req.params.gameid;
+        const gamePassword = req.params.gamepassword;
 
-        let gameId;
-        let isUnique = false;
-        while (isUnique === false) {
-            const randStr = randomstring.generate({ length: randStrLength, charset: 'alphanumeric' });
-            gameId = gameName + '-' + randStr;
-            const result = await dbGame.checkGameId(gameId);
-            result === EXISTS ? (isUnique = false) : (isUnique = true);
-        }
-
-        const io = req.app.locals.io;
-        io.emit('conc', { data: 'data' });
-
-        const newGame = {
-            gameId: gameId,
-            gamePassword: gamePassword,
-            players: {},
-        };
-
-        const game = await dbGame.createGame(newGame);
+        const game = await dbGame.getGame(gameId, gamePassword);
 
         if (game.status === SUCCESS) {
-            res.status(201).json({ status: SUCCESS, data: game.data });
+            const bundles = getWordBundles();
+            game.data.wordGroups = bundles;
+            res.status(200).json({ status: SUCCESS, data: game.data });
         } else {
-            res.status(500).json({ status: FAIL, msg: game.msg });
+            res.status(404).json({ status: FAIL, msg: game.msg });
+        }
+    } catch (e) {
+        res.status(500).json({ status: FAIL, error: e });
+    }
+});
+
+game.post('/', async (req, res) => {
+    try {
+        const gameId = req.body.gameId;
+        const gamePassword = req.body.gamePassword;
+
+        const result = await dbGame.checkGameId(gameId);
+        console.log('CHECKING ID RES: ', result);
+        if (result.status === EMPTY) {
+            const newGame = {
+                gameId: gameId,
+                gamePassword: gamePassword,
+            };
+
+            const game = await dbGame.createGame(newGame);
+
+            if (game.status === SUCCESS) {
+                const bundles = getWordBundles();
+                game.data.wordGroups = bundles;
+                res.status(201).json({ status: SUCCESS, data: game.data });
+            } else {
+                res.status(500).json({ status: FAIL, msg: game.msg });
+            }
+        } else {
+            res.status(409).json({
+                status: FAIL,
+                msg: 'A game with that name already exists, please try another name',
+            });
         }
     } catch (e) {
         res.status(500).json({ status: FAIL, error: e });
