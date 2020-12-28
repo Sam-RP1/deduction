761a138e63d07d3290572d2df954984f6519496e
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-12-28 19:33:23 +0000

    created useSocket, initialises client side socket.io and returns a ref to it

---
+++ src/frontend/hooks/useSocket.js
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import socketIOClient from 'socket.io-client';
+
+// Redux Action Types
+import * as player from '../store/actions/player';
+
+const useSocket = () => {
+    // Redux Actions
+    const dispatch = useDispatch();
+    const submitPlayerId = useCallback((id) => dispatch(player.setPlayerId(id)), [dispatch]); // eslint-disable-line
+
+    // Vars & Refs
+    const socketRef = useRef();
+    const SOCKET_SERVER_URL = 'http://localhost:4000';
+
+    useEffect(() => {
+        socketRef.current = socketIOClient(SOCKET_SERVER_URL, {});
+
+        socketRef.current.on('connect', () => {
+            submitPlayerId(socketRef.current.id);
+        });
+
+        return () => {
+            socketRef.current.disconnect();
+            console.log('disconnect from socket');
+        };
+    }, []);
+
+    return { socketRef };
+};
+
+export default useSocket;
