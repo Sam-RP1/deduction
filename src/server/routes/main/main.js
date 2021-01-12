2e86948560f023c2d6054b41176cd04a0b80dc9a
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-01-12 18:58:24 +0000

    update: added var to decide what path to use

--- src/server/routes/main/main.js
+++ src/server/routes/main/main.js
@@ -8,6 +8,8 @@ module.exports = main;
 
 main.use(cors({ origin: true }));
 
+const publicPath = process.env.NODE_ENV === 'PRODUCTION' ? '../../public' : 'public';
+
 main.get(['/', '/creategame', '/joingame', '/game'], (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, publicPath, 'index.html'));
 });
