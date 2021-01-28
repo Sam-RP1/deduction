e04ee530ee28d0f9fccbb9fb639b79b7ad47c17e
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-01-28 18:04:51 +0000

    update: added JsDoc and proptypes updated

--- src/frontend/components/Game/Game.js
+++ src/frontend/components/Game/Game.js
@@ -1,73 +1,113 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// Imports
-// import LoadingIndicator from '../UI/LoadingIndicator/LoadingIndicator'
-import Gameboard from './Gameboard/Gameboard';
-import GameControls from './GameControls/GameControls';
-
 // Styles
 import '../../styles/root.scss';
 import './Game.scss';
 
-// Presentational Component
+// Imports
+import Gameboard from './Gameboard/Gameboard';
+import GameControls from './Controls/GameControls/GameControls';
+import TeamControls from './Controls/TeamControls/TeamControls';
+import RoleControls from './Controls/RoleControls/RoleControls';
+import BundleControls from './Controls/BundleControls/BundleControls';
+
+/**
+ * Presentational component for the Game container (Page / View).
+ * @function Game
+ * @param {object}  props - React props.
+ * @prop {function} props.addCustomWordHandler - Function allowing client to add a custom word.
+ * @prop {array}    props.blueTeam - The clients on the blue team.
+ * @prop {object}   props.customWordError - Error to be displayed if one exists.
+ * @prop {array}    props.customWords - Client entered custom words.
+ * @prop {function} props.endTurn - Function that ends a teams turn.
+ * @prop {object}   props.gameError - Object containing an error received from the server.
+ * @prop {function} props.guess - Function placed on each gameboard grid block to allow guesses to be made by the client.
+ * @prop {string}   props.isGameOver - String indicating if the game is over and which team has won.
+ * @prop {string}   props.joinCode - Code clients can use to join this specific game.
+ * @prop {function} props.newGame - Function allowing client to generate a new game.
+ * @prop {string}   props.playerRole - The clients role.
+ * @prop {string}   props.playerTeam - String containing the clients team.
+ * @prop {func}     props.randomiseTeams - Function that emits a socket event to randomise the teams.
+ * @prop {array}    props.redTeam - The clients on the red team.
+ * @prop {function} props.removeCustomWord - Function allowing client to remove a custom word.
+ * @prop {object}   props.score - Contains the score for the blue team and the red team.
+ * @prop {function} props.selectRole - Function that emits a socket event to assign the client the selected role.
+ * @prop {function} props.selectTeam - Function that emits a socket event to put the client on the selected team.
+ * @prop {function} props.selectWordBundle - Function that allows clients to select a word bundle for play.
+ * @prop {string}   props.turn - The current teams turn.
+ * @prop {array}    props.unassigned - The clients that are not on a team.
+ * @prop {function} props.useCustomWords - Function that emits a socket event to use the currently entered custom words for play.
+ * @prop {array}    props.wordsArr - Word data used to generate the gameboard grid.
+ * @prop {string}   props.wordBundle - The currently selected word bundle.
+ * @prop {array}    props.wordBundles - The currently selectable word bundles as strings.
+ * @returns {JSX}
+ */
 const Game = (props) => {
     return (
         <section className='game'>
             <div className='game__board'>
-                <div className='game__board-container'>
-                    <div className='game__board-container__information'>
-                        <p>
-                            <span className='red-txt'>{props.score.red}</span> -{' '}
-                            <span className='blue-txt'>{props.score.blue}</span>
-                        </p>
-                        <p>
-                            <span className={props.turn === 'blue' ? 'blue-txt' : 'red-txt'}>Turn</span>
-                        </p>
-                    </div>
-                    <Gameboard
-                        turn={props.turn}
-                        playerTeam={props.playerTeam}
-                        playerRole={props.playerRole}
-                        wordsArr={props.wordsArr}
-                        guess={props.guess}
-                        isGameOver={props.isGameOver}
-                        gameError={props.gameError}
-                    />
-                    {props.isGameOver !== null && (
-                        <div
-                            className={
-                                'game__board-container__game-over ' +
-                                (props.isGameOver === 'blue' ? 'blue-bg' : 'red-bg')
-                            }
-                        >
-                            <h1>--- Game Over ---</h1>
-                            <h1>{props.isGameOver} team wins!</h1>
-                        </div>
-                    )}
-                    {props.gameError}
+                <div className='game__board__information'>
+                    <p>
+                        <span className='red-txt'>{props.score.red}</span> -{' '}
+                        <span className='blue-txt'>{props.score.blue}</span>
+                    </p>
+                    <p>
+                        <span className={props.turn === 'blue' ? 'blue-txt' : 'red-txt'}>Turn</span>
+                    </p>
                 </div>
+                <Gameboard
+                    turn={props.turn}
+                    playerTeam={props.playerTeam}
+                    playerRole={props.playerRole}
+                    wordsData={props.wordsArr}
+                    guess={props.guess}
+                    isGameOver={props.isGameOver}
+                    gameError={props.gameError}
+                />
+                {props.isGameOver !== null && (
+                    <div className={'game__board__game-over ' + (props.isGameOver === 'blue' ? 'blue-bg' : 'red-bg')}>
+                        <h1>--- Game Over ---</h1>
+                        <h1>{props.isGameOver} team wins!</h1>
+                    </div>
+                )}
+                {props.gameError}
+            </div>
+            <div className='game__controls'>
                 <GameControls
+                    wordBundle={props.wordBundle}
+                    customWords={props.customWords}
                     newGame={props.newGame}
                     joinCode={props.joinCode}
                     endTurn={props.endTurn}
+                    playerTeam={props.playerTeam}
                     turn={props.turn}
+                />
+
+                <TeamControls
+                    randomiseTeams={props.randomiseTeams}
+                    selectTeam={props.selectTeam}
+                    clientTeam={props.playerTeam}
                     blueTeam={props.blueTeam}
                     redTeam={props.redTeam}
                     unassigned={props.unassigned}
-                    randomiseTeams={props.randomiseTeams}
-                    selectTeam={props.selectTeam}
+                />
+
+                <RoleControls
                     selectRole={props.selectRole}
-                    playerTeam={props.playerTeam}
-                    playerRole={props.playerRole}
+                    clientTeam={props.playerTeam}
+                    clientRole={props.playerRole}
+                />
+
+                <BundleControls
                     wordBundles={props.wordBundles}
                     wordBundle={props.wordBundle}
                     selectWordBundle={props.selectWordBundle}
-                    customWords={props.customWords}
                     addCustomWordHandler={props.addCustomWordHandler}
+                    customWords={props.customWords}
+                    customWordError={props.customWordError}
                     removeCustomWord={props.removeCustomWord}
                     useCustomWords={props.useCustomWords}
-                    customWordError={props.customWordError}
                 />
             </div>
         </section>
@@ -75,38 +115,54 @@ const Game = (props) => {
 };
 
 Game.propTypes = {
-    newGame: PropTypes.func,
-    joinCode: PropTypes.string,
-    endTurn: PropTypes.func,
-    score: PropTypes.object,
-    turn: PropTypes.string,
-    // Teams & Roles
-    blueTeam: PropTypes.array,
-    redTeam: PropTypes.array,
-    unassigned: PropTypes.array,
-    randomiseTeams: PropTypes.func,
-    selectTeam: PropTypes.func,
-    selectRole: PropTypes.func,
-    playerTeam: PropTypes.string,
-    playerRole: PropTypes.string,
-    // Words
-    wordBundles: PropTypes.array,
-    wordBundle: PropTypes.string,
-    selectWordBundle: PropTypes.func,
-    wordsArr: PropTypes.array,
-    customWords: PropTypes.array,
     addCustomWordHandler: PropTypes.func,
-    removeCustomWord: PropTypes.func,
-    useCustomWords: PropTypes.func,
+    blueTeam: PropTypes.arrayOf(
+        PropTypes.shape({
+            playerId: PropTypes.string,
+            playerName: PropTypes.string,
+            team: PropTypes.string,
+            role: PropTypes.string,
+        })
+    ),
     customWordError: PropTypes.object,
-    // Guess
-    guess: PropTypes.func,
-    // Game over
-    isGameOver: PropTypes.string,
-    // Game Error
+    customWords: PropTypes.arrayOf(PropTypes.string),
+    endTurn: PropTypes.func,
     gameError: PropTypes.object,
+    guess: PropTypes.func,
+    isGameOver: PropTypes.string,
+    joinCode: PropTypes.string,
+    newGame: PropTypes.func,
+    playerRole: PropTypes.string,
+    playerTeam: PropTypes.string,
+    randomiseTeams: PropTypes.func,
+    redTeam: PropTypes.arrayOf(
+        PropTypes.shape({
+            playerId: PropTypes.string,
+            playerName: PropTypes.string,
+            team: PropTypes.string,
+            role: PropTypes.string,
+        })
+    ),
+    removeCustomWord: PropTypes.func,
+    score: PropTypes.objectOf({ blue: PropTypes.string, red: PropTypes.string }),
+    selectRole: PropTypes.func,
+    selectTeam: PropTypes.func,
+    selectWordBundle: PropTypes.func,
+    turn: PropTypes.string,
+    unassigned: PropTypes.arrayOf(
+        PropTypes.shape({
+            playerId: PropTypes.string,
+            playerName: PropTypes.string,
+            team: PropTypes.string,
+            role: PropTypes.string,
+        })
+    ),
+    useCustomWords: PropTypes.func,
+    wordsArr: PropTypes.arrayOf(PropTypes.string),
+    wordBundle: PropTypes.string,
+    wordBundles: PropTypes.arrayOf(PropTypes.string),
 };
 
 Game.defaultProps = {};
 
-export default Game;
+export default React.memo(Game);
